f57f22f5ec449fd74ed428d5fe2a8f2ebfb4cd3f
Prashant S Laxmeshwarmath <95927107+Prashant-SL@users.noreply.github.com>
2023-08-15 18:37:00 +0530

    [PrashantSL] Added transaction category reuseable component (#1)
    Co-authored-by: Prashanth <prashanth@IBOs-MacBook-Pro-3.local>

---
+++ README.md
@@ -0,0 +1,3 @@
+Track Xpense Web App
+
+This is built with the design inspiration from this free <a href="https://www.figma.com/file/zC7UBobrxKKL32sDU9eKZZ/Income-%26-Expense-Manager-(Community)?type=design&node-id=2-18&mode=design">figma design</a>
--- client/src/router.jsx
+++ client/src/router.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AddTransaction from "./components/AddTransaction";
 import Dashboard from "./components/Dashboard";
+import Income from "./components/Income";
+import Expense from "./components/Expense";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/add-transaction" element={<AddTransaction />}>
-        Add Transaction
-      </Route>
       <Route path="/" element={<Dashboard />}>
         Home
+      </Route>
+      <Route path="add-transaction" element={<AddTransaction />}>
+        <Route path="income" element={<Income />} />
+        <Route path="expense" element={<Expense />} />
       </Route>
     </Routes>
   );
