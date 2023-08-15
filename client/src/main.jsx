57ef3452ff48a00bdbf9e3ace886004e06ebd110
Prashanth <prashanth@IBOs-MacBook-Pro-3.local>
2023-08-15 13:39:53 +0530

    [PrashantSL] Initial setup

---
+++ client/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <h1 className="text-3xl font-bold underline border-2">Hello world!</h1>
+  </React.StrictMode>
+);
