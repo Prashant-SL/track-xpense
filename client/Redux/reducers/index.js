a068809d1d25da99dee1c567e6f851df5d1f908a
Prashant <prashanth@IBOs-MacBook-Pro-3.local>
2023-09-01 19:03:37 +0530

    [PrashantSL] Removed redux code

--- client/Redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import { transactionsReducer, mainBalanceReducer } from "./transactionsReducer";
-const reducers = combineReducers({
-  transactions: transactionsReducer,
-  balance: mainBalanceReducer,
-});
-export default reducers;
