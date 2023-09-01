d68b9822f2f365db400aa136309de215e9a93056
Prashant <prashanth@IBOs-MacBook-Pro-3.local>
2023-09-01 14:22:16 +0530

    [PrashantSL] Deployment related changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require("path");
 const connectDb = require("./configs/db");
 
 app.use(express.json());
@@ -12,6 +13,12 @@ connectDb();
 // Transaction routes
 app.use("/api/v1/transactions", require("./routes/transaction.route"));
 app.use("/api/v1/user", require("./routes/user.route"));
+
+app.use(express.static(path.join(__dirname, "./client/dist")));
+
+app.get("*", async (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
