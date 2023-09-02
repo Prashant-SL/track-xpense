de1600bca1351fa733c2ac8e3a98e5d3de45c420
Prashant <prashanth@IBOs-MacBook-Pro-3.local>
2023-09-02 15:36:31 +0530

    [PrashantSL] Removed unnecessary useEffect

--- client/pages/Dashboard.jsx
+++ client/pages/Dashboard.jsx
@@ -1,19 +1,13 @@
 import { useQuery } from "react-query";
-import { useEffect } from "react";
 import TransactionButton from "../src/components/transaction/TransactionButton";
 import TransactionList from "../src/components/transaction/TransactionList";
 import Skeleton from "../src/components/Skeleton";
 import * as URLHelpers from "../src/helpers/URLHelpers";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { NoContentSVG } from "../src/svg/index.js";
 import NoContent from "../src/components/NoContent";
 
 const Dashboard = () => {
-  useEffect(() => {
-    if (!localStorage.getItem("token")) window.location.href = "/login";
-  });
-
   const headers = {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("token")}`,
